024f5de846b7751ee9a8c0c9154e2fea751ff486
Sandeep Yadav <sdyadav7049@gmail.com>
2025-08-12 02:00:21 +0530

    done with backened

--- productcatalog/src/main/java/com/product/productcatalog/controller/CatagoryController.java
+++ productcatalog/src/main/java/com/product/productcatalog/controller/CatagoryController.java
@@ -3,6 +3,7 @@ package com.product.productcatalog.controller;
 import com.product.productcatalog.entity.Category;
 import com.product.productcatalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CatagoryController {
 
     @Autowired private CategoryService catagoryService;
--- productcatalog/src/main/java/com/product/productcatalog/controller/ProductController.java
+++ productcatalog/src/main/java/com/product/productcatalog/controller/ProductController.java
@@ -3,15 +3,13 @@ package com.product.productcatalog.controller;
 import com.product.productcatalog.entity.Product;
 import com.product.productcatalog.service.ProdcutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     @Autowired
